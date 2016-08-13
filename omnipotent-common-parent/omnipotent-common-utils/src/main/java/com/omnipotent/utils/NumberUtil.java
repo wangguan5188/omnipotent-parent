6170b623abc336541a9e6d53b77f50e4598e6fdb
wangguan5188 <572119023@qq.com>
2016-08-13 17:46:12 +0800

    分布式项目：初始化架构

---
+++ omnipotent-common-parent/omnipotent-common-utils/src/main/java/com/omnipotent/utils/NumberUtil.java
@@ -0,0 +1,37 @@
+package com.omnipotent.utils;
+
+/**
+ * 数字处理工具类
+ * 
+ * @author zhangtb
+ * @date 2016-8-3 16:21:26
+ * @since 1.0.0
+ */
+public class NumberUtil {
+	
+	/**
+	 * 金额单位转换（元 --> 分）：待实现
+	 * @return
+	 */
+	public int yuanToFen() {
+		int result = 0;
+		
+		return result;
+	}
+	
+	/**
+	 * 金额单位转换（分 --> 元）：待实现
+	 * 
+	 * @return
+	 */
+	public int fenToYuan() {
+		int result = 0;
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
